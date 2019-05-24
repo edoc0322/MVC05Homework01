a2759a0a3748f77cb5d2f587e04dadccea91e1c9
HaroldWu <haroldwu@kpmg.com.tw>
2019-05-24 20:52:40 +0800

    更新部分內容 and 充滿疑問~~~

--- MVC05Homework01/Controllers/客戶資料Controller.cs
+++ MVC05Homework01/Controllers/客戶資料Controller.cs
@@ -28,10 +28,10 @@ namespace MVC05Homework01.Controllers
 
         [取得分類清單]
         // GET: 客戶資料
-        public ActionResult Index(int? 分類篩選, string 客戶名稱,  string currentSort, string sortOrder = "客戶名稱")
+        public ActionResult Index(int? 分類篩選, string 客戶名稱, string currentSort, string sortOrder = "客戶名稱")
         {
             ViewBag.CurrentSort = sortOrder.Equals(currentSort) ? null : sortOrder;
-            var temp = rep.AllOfQuery(分類篩選,客戶名稱).Sort(sortOrder, currentSort)
+            var temp = rep.AllOfQuery(分類篩選, 客戶名稱).Sort(sortOrder, currentSort)
                        .Select(x => new ViewModels.客戶資料ViewModel()
                        {
                            Id = x.Id,
@@ -47,18 +47,18 @@ namespace MVC05Homework01.Controllers
             return View(temp);
         }
 
-        public FileResult Download(int? 分類篩選,string 客戶名稱, string currentSort, string sortOrder = "客戶名稱")
+        public FileResult Download(int? 分類篩選, string 客戶名稱, string currentSort, string sortOrder = "客戶名稱")
         {
-            var 客戶資料 = rep.AllOfQuery(分類篩選,客戶名稱).Sort(sortOrder, currentSort).Select(x =>
-            new ViewModels.客戶資料ExportViewModel
-            {
-                客戶名稱 = x.客戶名稱,
-                統一編號 = x.統一編號,
-                Email = x.Email,
-                傳真 = x.傳真,
-                電話 = x.電話,
-                地址 = x.地址
-            });
+            var 客戶資料 = rep.AllOfQuery(分類篩選, 客戶名稱).Sort(sortOrder, currentSort).Select(x =>
+             new ViewModels.客戶資料ExportViewModel
+             {
+                 客戶名稱 = x.客戶名稱,
+                 統一編號 = x.統一編號,
+                 Email = x.Email,
+                 傳真 = x.傳真,
+                 電話 = x.電話,
+                 地址 = x.地址
+             });
             return new DownloadController().ExportExcelV2(客戶資料.ListToDataTable(), "客戶資料.xlsx", "Data");
             //return new DownloadController.ExportExcelResult
             //{
@@ -72,9 +72,8 @@ namespace MVC05Homework01.Controllers
         public ActionResult Details(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+
             var 客戶資料 = rep.Find(id.Value);
             客戶資料.客戶聯絡人 = rep聯絡人.All().Where(x => x.客戶Id == id.Value).ToList();
 
@@ -166,28 +165,49 @@ namespace MVC05Homework01.Controllers
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,客戶分類,客戶名稱,統一編號,電話,傳真,地址,Email,已刪除")] ViewModels.客戶資料ViewModel 客戶資料ViewModel)
+        public ActionResult Edit(int id, FormCollection form)
+        //public ActionResult Edit([Bind(Include = "Id,客戶分類,客戶名稱,統一編號,電話,傳真,地址,Email,已刪除")] ViewModels.客戶資料ViewModel 客戶資料ViewModel)
         {
-            if (ModelState.IsValid)
+            var 客戶資料 = rep.Find(id);
+            if (TryUpdateModel<客戶資料>(客戶資料) && TryValidateModel(客戶資料))
             {
-                var 客戶資料 = new 客戶資料
-                {
-                    Id = 客戶資料ViewModel.Id,
-                    客戶分類 = (int)客戶資料ViewModel.客戶分類,
-                    客戶名稱 = 客戶資料ViewModel.客戶名稱,
-                    Email = 客戶資料ViewModel.Email,
-                    傳真 = 客戶資料ViewModel.傳真,
-                    地址 = 客戶資料ViewModel.地址,
-                    統一編號 = 客戶資料ViewModel.統一編號,
-                    電話 = 客戶資料ViewModel.電話,
-                    已刪除 = false
-                };
-
-                rep.UnitOfWork.Context.Entry(客戶資料).State = EntityState.Modified;
                 rep.UnitOfWork.Commit();
                 return RedirectToAction("Index");
             }
+            var 客戶資料ViewModel = new ViewModels.客戶資料ViewModel
+            {
+                客戶名稱 = 客戶資料.客戶名稱,
+                Email = 客戶資料.Email,
+                Id = 客戶資料.Id,
+                傳真 = 客戶資料.傳真,
+                地址 = 客戶資料.地址,
+                客戶分類 = (EnumModel.客戶分類)客戶資料.客戶分類,
+                已刪除 = 客戶資料.已刪除,
+                統一編號 = 客戶資料.統一編號,
+                電話 = 客戶資料.電話
+            };
             return View(客戶資料ViewModel);
+
+            //如果Input沒有 FormCollection 那TryUpdateModel就會進行模型驗證，如果有就不會，就要搭配TryValidateModel 做模型驗證。
+            //if (ModelState.IsValid)
+            //{
+            //    var 客戶資料 = new 客戶資料
+            //    {
+            //        Id = 客戶資料ViewModel.Id,
+            //        客戶分類 = (int)客戶資料ViewModel.客戶分類,
+            //        客戶名稱 = 客戶資料ViewModel.客戶名稱,
+            //        Email = 客戶資料ViewModel.Email,
+            //        傳真 = 客戶資料ViewModel.傳真,
+            //        地址 = 客戶資料ViewModel.地址,
+            //        統一編號 = 客戶資料ViewModel.統一編號,
+            //        電話 = 客戶資料ViewModel.電話,
+            //        已刪除 = false
+            //    };
+            //
+            //    rep.UnitOfWork.Context.Entry(客戶資料).State = EntityState.Modified;
+            //    rep.UnitOfWork.Commit();
+            //    return RedirectToAction("Index");
+            //}
         }
 
         // GET: 客戶資料/Delete/5
